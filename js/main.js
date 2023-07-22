c9539e3012df76953d97bfe26afdcd0b559665db
Fpelourson <fede_pelourson@hotmail.com>
2023-07-22 00:12:00 -0300

    correccion-tercera

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const nav = document.querySelector("#menu");
+const navb = document.querySelector("#menu");
+const abrirB = document.querySelector("#menu-bar-b");
 const abrir = document.querySelector("#menu-bar");
 const cerrar = document.querySelector("#hero");
+
 
 abrir.addEventListener('click', () => {
     nav.classList.add('visible');
